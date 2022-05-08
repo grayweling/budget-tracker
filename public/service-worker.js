085eb7f95f28b0ead029cd63374e3dd3bf9e9b07
grayweling <jy6228@yahoo.com>
2022-05-08 16:00:04 -0500

    fixing service worker to work offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,14 +3,14 @@ const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  "/index.html",
-  "/css/styles.css",
-  "/js/idb.js",
-  "/js/index.js",
+  "./index.html",
+  "./css/styles.css",
+  "./js/idb.js",
+  "./js/index.js",
 ];
 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
+self.addEventListener("install", function (event) {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
@@ -18,8 +18,8 @@ self.addEventListener("install", function (e) {
   );
 });
 
-self.addEventListener("activate", function (e) {
-  e.waitUntil(
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
     caches.keys().then(function (keyList) {
       let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
@@ -38,16 +38,16 @@ self.addEventListener("activate", function (e) {
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function(request) {
+self.addEventListener('fetch', function(event) {
+  console.log('fetch request : ' + event.request.url)
+  event.respondWith(
+    caches.match(event.request).then(function(request) {
       if(request) {
-        console.log('responding with cache: ' + e.request.url)
+        console.log('responding with cache: ' + event.request.url)
         return request
       } else {
-        console.log('file is not cached, fetching: ' + e.request.url)
-        return fetch(e.request)
+        console.log('file is not cached, fetching: ' + event.request.url)
+        return fetch(event.request)
       }
     })
   )
