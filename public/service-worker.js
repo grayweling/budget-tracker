aff5918d0fa877b27a8f37ccdbb1b2e4b5b1e0b8
grayweling <jy6228@yahoo.com>
2022-05-09 18:08:22 -0500

    pruned obsolete functions

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,13 +2,6 @@ const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-// const FILES_TO_CACHE = [
-//   "index.html",
-//   "css/styles.css",
-//   "js/idb.js",
-//   "js/index.js",
-//   "manifest.json",
-// ];
 
 const addResourcesToCache = async (resources) => {
     const cache = await caches.open(CACHE_NAME);
@@ -63,10 +56,6 @@ const deleteOldCaches = async () => {
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    // caches.open(CACHE_NAME).then(function (cache) {
-    //   console.log("installing cache : " + CACHE_NAME);
-    //   return cache.addAll(FILES_TO_CACHE);
-    // })
     addResourcesToCache([
         "./index.html",
         "./css/styles.css",
@@ -97,20 +86,6 @@ self.addEventListener("activate", function (event) {
   );
 });
 
-// self.addEventListener('fetch', function(event) {
-//   console.log('fetch request : ' + event.request.url)
-//   event.respondWith(
-//     caches.match(event.request).then(function(request) {
-//       if(request) {
-//         console.log('responding with cache: ' + event.request.url)
-//         return request
-//       } else {
-//         console.log('file is not cached, fetching: ' + event.request.url)
-//         return fetch(event.request)
-//       }
-//     })
-//   )
-// })
 
 self.addEventListener("fetch", (event) => {
     event.respondWith(
