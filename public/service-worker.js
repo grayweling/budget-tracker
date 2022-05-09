5f31f3104d91cfee9c9a7bc2154f2c24cbfaf8d7
grayweling <jy6228@yahoo.com>
2022-05-09 18:07:04 -0500

    FINALLY got offline working

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,6 +15,52 @@ const addResourcesToCache = async (resources) => {
     await cache.addAll(resources);
 }
 
+const putInCache = async (request, response) => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+    // First try to get the resource from the cache
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
+    }
+  
+    // Next try to get the resource from the network
+    try {
+      const responseFromNetwork = await fetch(request);
+      // response may be used only once
+      // we need to save clone to put one copy in cache
+      // and serve second one
+      putInCache(request, responseFromNetwork.clone());
+      return responseFromNetwork;
+    } catch (error) {
+      const fallbackResponse = await caches.match(fallbackUrl);
+      if (fallbackResponse) {
+        return fallbackResponse;
+      }
+      // when even the fallback response is not available,
+      // there is nothing we can do, but we must always
+      // return a Response object
+      return new Response('Network error happened', {
+        status: 408,
+        headers: { 'Content-Type': 'text/plain' },
+      });
+    }
+  };
+
+const deleteCache = async key => {
+    await caches.delete(key);
+}
+
+const deleteOldCaches = async () => {
+    const cacheKeepList = [CACHE_NAME];
+    const keyList = await caches.keys();
+    const cachesToDelete = keyList.filter(key => !cacheKeepList.includes(key));
+    await Promise.all(cachesToDelete.map(key => deleteCache(key)));
+}
+
 self.addEventListener("install", function (event) {
   event.waitUntil(
     // caches.open(CACHE_NAME).then(function (cache) {
@@ -22,11 +68,11 @@ self.addEventListener("install", function (event) {
     //   return cache.addAll(FILES_TO_CACHE);
     // })
     addResourcesToCache([
-        "index.html",
-        "css/styles.css",
-        "js/idb.js",
-        "js/index.js",
-        "manifest.json",
+        "./index.html",
+        "./css/styles.css",
+        "./js/idb.js",
+        "./js/index.js",
+        "./manifest.json",
     ])
   );
 });
@@ -51,17 +97,26 @@ self.addEventListener("activate", function (event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log('fetch request : ' + event.request.url)
-  event.respondWith(
-    caches.match(event.request).then(function(request) {
-      if(request) {
-        console.log('responding with cache: ' + event.request.url)
-        return request
-      } else {
-        console.log('file is not cached, fetching: ' + event.request.url)
-        return fetch(event.request)
-      }
-    })
-  )
-})
+// self.addEventListener('fetch', function(event) {
+//   console.log('fetch request : ' + event.request.url)
+//   event.respondWith(
+//     caches.match(event.request).then(function(request) {
+//       if(request) {
+//         console.log('responding with cache: ' + event.request.url)
+//         return request
+//       } else {
+//         console.log('file is not cached, fetching: ' + event.request.url)
+//         return fetch(event.request)
+//       }
+//     })
+//   )
+// })
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        cacheFirst({
+            request: event.request,
+            fallbackUrl: "index.html",
+        })
+    );
+});
