72bd8aa46d19cba2b2b29d2854e30282177d4ece
grayweling <jy6228@yahoo.com>
2022-05-09 23:39:59 -0500

    offline functionality is now properly functioning

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,15 +1,22 @@
-let db;
+const indexedDB = 
+    window.indexedDB || 
+    window.mozIndexedDB || 
+    window.webkitIndexedDB || 
+    window.msIndexedDB ||
+    window.shimIndexedDB;
 
+let db;
 const request = indexedDB.open("budget_tracker", 1);
 
 request.onupgradeneeded = function (event) {
-  const db = event.target.result;
+  let db = event.target.result;
   db.createObjectStore("new_txn", { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
   db = event.target.result;
 
+  //check if app is online before reading from db
   if (navigator.onLine) {
     uploadTxn();
   }
@@ -19,25 +26,21 @@ request.onerror = function (event) {
   console.log(event.target.errorCode);
 };
 
-
 function saveRecord(record) {
   const txn = db.transaction(["new_txn"], "readwrite");
-
   const txnObjectStore = txn.objectStore("new_txn");
 
   txnObjectStore.add(record);
 }
-
+//when online this happens
 function uploadTxn() {
   const txn = db.transaction(["new_txn"], "readwrite");
-
   const txnObjectStore = txn.objectStore("new_txn");
-
   const getAll = txnObjectStore.getAll();
 
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
-      fetch("/api/transaction", {
+      fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
@@ -45,22 +48,18 @@ function uploadTxn() {
           "Content-Type": "application/json",
         },
       })
-        .then((response) => response.json())
-        .then((serverResponse) => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
-          }
-          const txn = db.transaction(["new_txn"], "readwrite");
-          const txnObjectStore = txn.objectStore("new_txn");
-          txnObjectStore.clear();
-
-          alert("All saved transactions have been submitted");
+        .then((response) => {
+            return response.json();
         })
-        .catch((err) => {
-          console.log(err);
-        });
+        .then(() => {
+            //delete records if successful
+            const txn = db.transaction(["new_txn"], "readwrite");
+            const txnObjectStore = txn.objectStore("new_txn");
+            txnObjectStore.clear();
+        })
     }
   };
 }
 
+//listen for app coming back online
 window.addEventListener('online', uploadTxn);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,25 @@
 const APP_PREFIX = "BudgetTracker-";
-const VERSION = "version_01";
+const VERSION = "v1";
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
-
+const FILES_TO_CACHE = [
+    "/",
+    "./index.html",
+    "./css/styles.css",
+    "./js/idb.js",
+    "./js/index.js",
+    "./manifest.json",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
+  
+]
 const addResourcesToCache = async (resources) => {
     const cache = await caches.open(CACHE_NAME);
     await cache.addAll(resources);
@@ -43,28 +60,14 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
     }
   };
 
-const deleteCache = async key => {
-    await caches.delete(key);
-}
-
-const deleteOldCaches = async () => {
-    const cacheKeepList = [CACHE_NAME];
-    const keyList = await caches.keys();
-    const cachesToDelete = keyList.filter(key => !cacheKeepList.includes(key));
-    await Promise.all(cachesToDelete.map(key => deleteCache(key)));
-}
-
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    addResourcesToCache([
-        "./index.html",
-        "./css/styles.css",
-        "./js/idb.js",
-        "./js/index.js",
-        "./manifest.json",
-    ])
-  );
-});
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    )
+})
 
 self.addEventListener("activate", function (event) {
   event.waitUntil(
@@ -87,11 +90,39 @@ self.addEventListener("activate", function (event) {
 });
 
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(event.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
     event.respondWith(
-        cacheFirst({
-            request: event.request,
-            fallbackUrl: "index.html",
+        fetch(event.request).catch(function() {
+            return caches.match(event.request).then(function(response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/");
+                }
+            });
         })
-    );
+    )
 });
