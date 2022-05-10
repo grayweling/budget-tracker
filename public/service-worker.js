c856b3f99a8f117909f78659268859d252ee15d7
grayweling <jy6228@yahoo.com>
2022-05-09 23:45:44 -0500

    pruned old code

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,45 +20,7 @@ const FILES_TO_CACHE = [
     "./icons/icon-512x512.png"
   
 ]
-const addResourcesToCache = async (resources) => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.addAll(resources);
-}
 
-const putInCache = async (request, response) => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.put(request, response);
-};
-
-const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-    // First try to get the resource from the cache
-    const responseFromCache = await caches.match(request);
-    if (responseFromCache) {
-      return responseFromCache;
-    }
-  
-    // Next try to get the resource from the network
-    try {
-      const responseFromNetwork = await fetch(request);
-      // response may be used only once
-      // we need to save clone to put one copy in cache
-      // and serve second one
-      putInCache(request, responseFromNetwork.clone());
-      return responseFromNetwork;
-    } catch (error) {
-      const fallbackResponse = await caches.match(fallbackUrl);
-      if (fallbackResponse) {
-        return fallbackResponse;
-      }
-      // when even the fallback response is not available,
-      // there is nothing we can do, but we must always
-      // return a Response object
-      return new Response('Network error happened', {
-        status: 408,
-        headers: { 'Content-Type': 'text/plain' },
-      });
-    }
-  };
 
 self.addEventListener("install", function(event) {
     event.waitUntil(
